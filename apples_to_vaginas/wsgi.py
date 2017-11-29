4607650dd943330a48c1c294a8bee2ad57a55fc0
Pedro Martinez <pedro.martinez@hellolabs.com>
2017-11-28 21:53:50 -0500

    Fixing typo

--- apples_to_vaginas/wsgi.py
+++ apples_to_vaginas/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apples_to_vaginas_dos.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apples_to_vaginas.settings")
 
 application = get_wsgi_application()
